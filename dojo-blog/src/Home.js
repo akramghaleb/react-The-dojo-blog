80e134ec81b8e1d1cde10f3264706a00e4a5ace5
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-17 13:55:44 +0300

    props

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 
 const HomePage = () => {
     const [blogs] = useState([
@@ -9,12 +10,7 @@ const HomePage = () => {
 
     return ( 
         <div className="home">
-            {blogs.map(blog => (
-                <div className="blog-preview" key={blog.id} >
-                <h2>{ blog.title }</h2>
-                <p>Written by { blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title='All Blogs'/>
         </div>
      );
 }
