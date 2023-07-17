df0fade816a255b70e5174df7fbb12f78c5b902e
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-17 11:38:10 +0300

    Dynamic Values in templates

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "welcome to new blog";
+  const likes = 50;
+  const googleLink = "http://google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Linked  { likes } times</p>
+        <a href={googleLink}> Go to Google</a>
+      </div>
     </div>
   );
 }
