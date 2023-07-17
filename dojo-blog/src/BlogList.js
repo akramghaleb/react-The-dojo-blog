62231a3393f0e80847d64f0dc6eb937910a53b89
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-17 14:19:06 +0300

    Functions as Props

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,12 +1,13 @@
-const BlogList = ({blogs , title}) => {
+const BlogList = ({blogs , title ,deleteBlog}) => {
 
     return ( 
         <div className="blog-list">
             <h2>{ title }</h2>
             {blogs.map(blog => (
                 <div className="blog-preview" key={blog.id} >
-                <h2>{ blog.title }</h2>
-                <p>Written by { blog.author }</p>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                    <button onClick={() => deleteBlog(blog.id)} >Delete Blog</button>
                 </div>
             ))}
         </div>
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,15 +2,23 @@ import {useState} from 'react';
 import BlogList from './BlogList';
 
 const HomePage = () => {
-    const [blogs] = useState([
+    const [blogs ,setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
       ])
 
+      const deleteBlog = (id) => {
+        setBlogs(blogs.filter((blog) => blog.id !== id))
+      }
+
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title='All Blogs'/>
+            <BlogList blogs={blogs} title='All Blogs' deleteBlog={deleteBlog}/>
+            <BlogList 
+                deleteBlog={deleteBlog}
+                blogs={blogs.filter((blog)=> blog.author === 'mario')} 
+                title="Mario's Blogs"/>
         </div>
      );
 }
