6900949c3e2d0c9131d646f9d107758c9bb3b53b
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-17 15:05:51 +0300

    Conditional Loading Message

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,6 +3,7 @@ import BlogList from './BlogList';
 
 const HomePage = () => {
     const [blogs ,setBlogs] = useState(null)
+    const [isPending ,setIsPending] = useState(true)
 
       const deleteBlog = (id) => {
         setBlogs(blogs.filter((blog) => blog.id !== id))
@@ -16,12 +17,14 @@ const HomePage = () => {
             })
             .then((data)=>{
                 setBlogs(data);
+                setIsPending(false);
             });
 
       } , []);
 
     return ( 
         <div className="home">
+            {isPending && <div>Loading ...</div>}
             {blogs && <BlogList blogs={blogs} title='All Blogs' deleteBlog={deleteBlog}/>}
         </div>
      );
