03f341e1a7bf7f051665ce6d3fa1194e312717b3
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-17 14:24:45 +0300

    useEffect

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState , useEffect} from 'react';
 import BlogList from './BlogList';
 
 const HomePage = () => {
@@ -11,6 +11,10 @@ const HomePage = () => {
       const deleteBlog = (id) => {
         setBlogs(blogs.filter((blog) => blog.id !== id))
       }
+
+      useEffect(()=>{
+        console.log(blogs);
+      });
 
     return ( 
         <div className="home">
