4bd3685f4cb30b38b569de666af11f11ba770363
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-17 13:03:50 +0300

    useState

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,17 @@
+import {useState} from 'react';
+
 const HomePage = () => {
+    const [name , setName] = useState('Akram');
+
+    const handleClick = () =>{
+        setName('Mohammed')
+    }
+
     return ( 
         <div className="home">
             <h1>Home Page</h1>
+            <p>{`Welcome ${ name }`}</p>
+            <button onClick = {handleClick}>Change Name</button>
         </div>
      );
 }
