7cf0549191cf3c4ed5cdee3b8f5d03b84bb66711
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-18 08:42:05 +0300

    Handling errors

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,6 +4,7 @@ import BlogList from './BlogList';
 const HomePage = () => {
     const [blogs ,setBlogs] = useState(null)
     const [isPending ,setIsPending] = useState(true)
+    const [error ,setError] = useState(null)
 
       const deleteBlog = (id) => {
         setBlogs(blogs.filter((blog) => blog.id !== id))
@@ -13,17 +14,26 @@ const HomePage = () => {
 
         fetch('http://localhost:8000/blogs')
             .then((res)=>{
+                if(! res.ok){
+                    throw Error('could not fetch the data');
+                }
                 return res.json();
             })
             .then((data)=>{
                 setBlogs(data);
                 setIsPending(false);
+                setError(null);
+            })
+            .catch(err=>{
+                setIsPending(false);
+                setError(err.message);
             });
 
       } , []);
 
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading ...</div>}
             {blogs && <BlogList blogs={blogs} title='All Blogs' deleteBlog={deleteBlog}/>}
         </div>
