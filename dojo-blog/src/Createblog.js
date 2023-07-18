8f3c192ccc3ddfc8b1da15863dcc470e9475e502
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-18 11:53:10 +0300

    Controlled Inputs (forms)

--- dojo-blog/src/Createblog.js
+++ dojo-blog/src/Createblog.js
@@ -1,9 +1,42 @@
+import { useState } from "react";
+
 const CreateBlog = () => {
-    return ( 
-        <div id="create_blog">
-            <h1>Create new blog</h1>
-        </div>
-     );
+    const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+      <p>{title}</p>
+      <p>{body}</p>
+      <p>{author}</p>
+    </div>
+  );
 }
  
 export default CreateBlog;
