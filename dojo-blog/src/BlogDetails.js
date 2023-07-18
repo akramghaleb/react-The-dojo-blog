c7242dcb27196d1e7e4f68a6b05db07ea4466d21
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-18 13:17:45 +0300

    Deleting Blogs

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,9 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams,useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+
+    const deleteBlog = () =>{
+        fetch(`http://localhost:8000/blogs/${id}`,{
+            method : 'DELETE'
+        }).then(res =>{
+            history.push('/');
+        })
+
+    }
 
     return ( 
         <div className="blog-details" >
@@ -14,6 +24,7 @@ const BlogDetails = () => {
                 <h2>{ blog.title }</h2>
                 <p>Written by { blog.author }</p>
                 <div>{ blog.body }</div>
+                <button onClick={deleteBlog}>Delete</button>
                 </article>
             )}
         </div>
