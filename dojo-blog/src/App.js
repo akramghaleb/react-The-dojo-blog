99fc5e799c43cc844a59b1a9e737335215c49d20
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-18 13:29:38 +0300

    404 Pages & Next Steps

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -4,6 +4,7 @@ import HomePage from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CreateBlog from './Createblog';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
 
@@ -21,6 +22,9 @@ function App() {
             </Route>
             <Route  path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route  path="*">
+              <NotFound />
             </Route>
             </Switch>
         </div>
